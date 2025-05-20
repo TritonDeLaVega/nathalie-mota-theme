02858d9b3761efb928f955f733b0cfb68ceac356
Jonathan <jacksaladin.js@gmail.com>
2025-05-20 14:20:30 +0200

    Ajout des dernières modifications du thème

--- index.php
+++ index.php
@@ -14,14 +14,70 @@
     </div>
 
     <div class="gallery-grid">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-0.jpeg" alt="photo 0">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-1.jpeg" alt="photo 1">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-2.jpeg" alt="photo 2">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-3.jpeg" alt="photo 3">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-4.jpeg" alt="photo 4">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-5.jpeg" alt="photo 5">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-6.jpeg" alt="photo 6">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-7.jpeg" alt="photo 7">
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-0.jpeg" alt="photo 0" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-1.jpeg" alt="photo 1" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-2.jpeg" alt="photo 2" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-3.jpeg" alt="photo 3" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-4.jpeg" alt="photo 4" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-5.jpeg" alt="photo 5" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-6.jpeg" alt="photo 6" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
+        <div class="gallery-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/nathalie-7.jpeg" alt="photo 7" />
+            <div class="overlay"></div>
+            <div class="icon-eye"></div>
+            <div class="icon-fullscreen"></div>
+            <div class="text-bottom-left">LE MENU</div>
+            <div class="text-bottom-right">CATÉGORIE</div>
+        </div>
     </div>
 
     <div class="load-more">
